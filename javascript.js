945b8dc6efae42f1aca3cf06997d86539318349e
cacheup <dcastelan613@gmail.com>
2022-06-20 23:47:25 -0400

    Create function for when an operator is clicked

--- javascript.js
+++ javascript.js
@@ -1,23 +1,47 @@
 const display = document.querySelector('.display');
 display.textContent = '0';
 
-let operand = NaN;
-let equalsBtnClicked = false;
-
+let displayModified = false;
 const digits = document.querySelectorAll('.digit');
 digits.forEach(digit => digit.addEventListener('click', populate));
 
+let operand = NaN;
+let operator = '';
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', operateCalc));
+
 function populate(e) {
-  if(equalsBtnClicked == true){
-    display.textContent = e.target.textContent;
-    equalsBtnClicked = false;
-  }
-  else{
+  if(displayModified == true){
     if(display.textContent == '0'){
       display.textContent = e.target.textContent;
     }
     else{
       display.textContent += e.target.textContent;
+    }
+  }
+  else{
+    display.textContent = e.target.textContent;
+    displayModified = true;
+  }
+}
+
+function operateCalc(e) {
+  if(displayModified == true){
+    if(isNaN(operand)){
+      operand = +display.textContent;
+      operator = e.target.textContent;
+    }
+    else{
+      operand = operate(operand, +display.textContent, operator);
+      display.textContent = operand.toString();
+      operator = e.target.textContent;
+    }
+    displayModified = false;
+  }
+  else{
+    if(isNaN(operand)){
+      operand = +display.textContent;
+      operator = e.target.textContent;
     }
   }
 }
@@ -44,9 +68,9 @@ function operate(num1,num2,operation) {
       return add(num1,num2);
     case '-':
       return subtract(num1,num2);
-    case '*':
+    case 'x':
       return multiply(num1,num2);
-    case '/':
+    case '÷':
       return divide(num1,num2);
   }
 }
