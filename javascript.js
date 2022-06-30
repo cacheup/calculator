91078bdcaaaf94246a2b50e877b6245467233410
cacheup <dcastelan613@gmail.com>
2022-06-29 20:35:07 -0400

    Prohibit user from dividing by zero

--- javascript.js
+++ javascript.js
@@ -105,7 +105,13 @@ function multiply(num1,num2) {
 }
 
 function divide(num1,num2) {
-  return num1 / num2;
+  if(num2 == 0){
+    alert("How dare you!");
+    return 0;
+  }
+  else{
+    return num1 / num2;
+  }
 }
 
 function operate(num1,num2,operation) {
